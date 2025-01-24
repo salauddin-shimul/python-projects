e91bb33e85d7541d25e75a5757026f553aced043
SM Salauddin Shimul <103127075+salauddin-shimul@users.noreply.github.com>
2025-01-24 18:35:16 +0000

    Made n1 and n2 floats and made sure the result is rounded to the nearest 3 decimal place

--- arithmetic-calculator.py
+++ arithmetic-calculator.py
@@ -1,22 +1,22 @@
 # Initializing operator and numbers
 operator = input("Enter your arithmatic operator (+ - * /): ")
-n1 = int(input("Enter 1st number: "))
-n2 = int(input("Enter 2nd number: "))
+n1 = float(input("Enter 1st number: "))
+n2 = float(input("Enter 2nd number: "))
 
 result = None
 
 # Main logic
 if operator == "+":
     result = n1 + n2
-    print(f"Your answer is: {result}.")
+    print(f"Your answer is: {round(result, 3)}.")
 elif operator == "-":
     result = n1 - n2
-    print(f"Your answer is: {result}.")
+    print(f"Your answer is: {round(result, 3)}.")
 elif operator == "*":
     result = n1 * n2
-    print(f"Your answer is: {result}.")
+    print(f"Your answer is: {round(result, 3)}.")
 elif operator == "/":
     result = n1 / n2
-    print(f"Your answer is: {result}.")
+    print(f"Your answer is: {round(result, 3)}.")
 else:
     print("Your operator is not valid!")
